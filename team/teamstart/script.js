7bc57741b8270be85a7703425e207b8f0fcdbefd
Lizzy Woodruff <woo19022@byui.edu>
2021-09-30 09:27:44 -0600

    Co-authored-by: Marsh, Jeff <the56@byui.edu>

---
+++ team/teamstart/script.js
@@ -0,0 +1,67 @@
+
+// most popular background color
+document.getElementById('pop').style.backgroundColor="#FFCF2D";
+
+document.getElementById("popsign").style.backgroundColor="#FFCF2D";
+appearIntervalLeft();
+
+appearIntervalMiddle();
+
+appearIntervalRight();
+
+function appearIntervalLeft()
+{
+    let card = document.getElementById("personal")
+    card.hidden = true;
+    setTimeout(() => {
+        card.hidden = false;
+        card.fadeIn("slow");
+    }, 1000);
+}
+function appearIntervalMiddle()
+{
+    let card = document.getElementById("business")
+    card.hidden = true
+    setTimeout(() => {
+        card.hidden = false;
+        card.fadeIn("slow");
+    }, 2000);
+}
+function appearIntervalRight()
+{
+    let card = document.getElementById("enterprise")
+    card.hidden = true;
+    setTimeout(() => {
+        card.hidden = false;
+        card.fadeIn("slow");
+    }, 3000);
+}
+
+colorLinks("#0000FF");
+
+function colorLinks(hex) {
+    var links = document.getElementsByTagName("a");
+    for (var i = 0; i < links.length; i++) {
+        if (links[i].href) {
+            links[i].style.backgroundColor = hex;
+        }
+    }
+}
+
+document.getElementById("bpricing").style.fontSize = "x-large";
+
+function openWindow() {
+    alert("You have been signed up for the program!");
+
+    var email = prompt("Email: ");
+    p = document.createElement("p");
+    p.innerHTML = email;
+    document.getElementById("email" + id).appendChild(p);
+
+}
+
+function setValue(value) {
+    document.getElementById('value').value = value;
+}
+
+
