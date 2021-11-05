3f19852346d4ab354432061695af767d9509aee0
Lizzy Woodruff <woo19022@byui.edu>
2021-11-05 11:48:21 -0600

    week 8

---
+++ lesson08/js/script.js
@@ -0,0 +1,37 @@
+var d = new Date();
+var n = d.getFullYear();
+document.getElementById('year').innerHTML= n;
+
+today = new Date;
+today.toLocaleString('default', {dateStyle: 'full'})
+document.getElementById('time').innerHTML= today;
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+
+  const an= document.querySelector('#banner');
+
+  const friday= new Date();
+  let dayOfWeek;
+  dayOfWeek=friday.getDay();
+  if (dayOfWeek == 5) {
+     an.classList.toggle("pancakes");
+  }
+  else{
+      an.classList.remove("pancakes");
+      an.style.display="none";
+  }
+  WebFont.load({
+    google: {
+      families: ['Droid Sans', 'Droid Serif']
+    }
+  });
+  function adjustRating(rating) {
+    document.getElementById('ratingvalue').innerHTML = rating;
+}
